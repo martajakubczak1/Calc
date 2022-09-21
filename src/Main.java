3a695d27aaa6655bffffb4d73a458d7253b0fe5c
martajakubczak1 <martajakubczak1>
2022-09-21 18:20:51 +0200

    Added calculates

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,29 @@
+import java.util.Scanner;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter first value");
+        int x = scanner.nextInt();
+        System.out.println("Enter second value");
+        int y = scanner.nextInt();
+        System.out.println("What do you want to do?\nSelect:\n1.addition\n2.substraction\n3.multiplication\n4.division");
+        int select = scanner.nextInt();
+        switch (select){
+            case 1:
+                System.out.println("Result " + (x+y));
+                break;
+            case 2:
+                System.out.println("Result " + (x-y));
+                break;
+            case 3:
+            System.out.println("Result " + (x*y));
+            break;
+            case 4:
+                System.out.println("Result " + (x/y));
+                break;
+
+        }
     }
 }
