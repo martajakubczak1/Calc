04d7af0c6f8d97521f23e041bd6c010e23c07580
martajakubczak1 <martajakubczak1>
2022-09-21 20:37:12 +0200

    Added addidtion method

---
+++ src/Calculator.java
@@ -0,0 +1,7 @@
+public class Calculator {
+    public int addition(int x, int y){
+        int z = x + y;
+        return z;
+
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,8 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        System.out.println("Enter first value");
-        float x = scanner.nextInt();
-        System.out.println("Enter second value");
-        float y = scanner.nextFloat();
-        System.out.println("What do you want to do?\nSelect:\n1.addition\n2.substraction\n3.multiplication\n4.division");
-        int select = scanner.nextInt();
-        switch (select){
-            case 1:
-                System.out.println("Result " + (x+y));
-                break;
-            case 2:
-                System.out.println("Result " + (x-y));
-                break;
-            case 3:
-            System.out.println("Result " + (x*y));
-            break;
-            case 4:
-                System.out.println("Result " + (x/y));
-                break;
-
-        }
+    public static void main(String[] args){
+        Calculator kalkulator1= new Calculator();
+        System.out.println(kalkulator1.addition(4,6));
     }
 }
