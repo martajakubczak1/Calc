28fe576862cccc901757914da532c6d75c0a677d
martajakubczak1 <martajakubczak1>
2022-09-21 20:43:43 +0200

    Added subtraction method

--- src/Calculator.java
+++ src/Calculator.java
@@ -2,6 +2,8 @@ public class Calculator {
     public int addition(int x, int y){
         int z = x + y;
         return z;
-
+    }
+    public int subtracion(int x, int y){
+        return x-y;
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args){
         Calculator kalkulator1= new Calculator();
         System.out.println(kalkulator1.addition(4,6));
+        System.out.println(kalkulator1.subtracion(8,4));
     }
 }
